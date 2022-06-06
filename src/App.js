33500d06f05a3eb11f3555f9df3a9cdb9ec31dcf
Sultaan-Ahmed <sultanduet2019@gmail.com>
2022-06-06 11:29:07 +0600

    third commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>This site is building soon....</h1>
+      <div className="container">
+        <h1>Welcome to Mariam's E-commerce web application Front-end.</h1>
+        <p>This is still in under construction.</p>
+
+        <p className="thanks">Thanks.</p>
+      </div>
     </>
   );
 }
