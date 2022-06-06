3d9d7d321e22231acda910f2c39555e827cb7085
Sultaan-Ahmed <sultanduet2019@gmail.com>
2022-06-06 11:40:06 +0600

    Provide some css fixation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
         <h1>Welcome to Mariam's E-commerce web application Front-end.</h1>
         <p>This is still in under construction.</p>
 
-        <p className="thanks">Thanks.</p>
+        <p className="thanks">Please keep patient. </p>
       </div>
     </>
   );
