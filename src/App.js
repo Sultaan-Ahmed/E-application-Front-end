a5fc12e85f5d5e13981b658f424acfff9da78074
Sultaan-Ahmed <sultanduet2019@gmail.com>
2022-06-07 19:38:05 +0600

    Working on products details page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
 import Footer from "./components/Layout/Footer/Footer";
 import Header from "./components/Layout/Header/Header";
-
+import ProductDetails from "./components/Product/ProductDetails";
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductDetails />} />
+      </Routes>
       <Footer />
     </>
   );
